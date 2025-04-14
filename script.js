d12eee4c304405cdfc82108a76e816216a205e24
Akki2004 <52844886+Akki2004@users.noreply.github.com>
2025-04-14 10:29:14 -0700

    index.htmlscript.jsstyle.css

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+document.getElementById("nameForm").addEventListener("submit", function(e) {
+    e.preventDefault(); // prevent page refresh
+    const name = document.getElementById("nameInput").value;
+    if (name.trim() === "") {
+      alert("Please enter a name!");
+    } else {
+      alert("Hello, " + name + "!");
+    }
+  });
+  
